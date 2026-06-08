7701570ce8c43d1610ee4e2c35288dba60826f40
Raxzell <rasya02347@gmail.com>
2026-06-09 06:44:54 +0700

    Add new routes for lifecycle and management of barang

--- routes/barang.js
+++ routes/barang.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { upload, tambahBarang, getBarang, getBarangUser } = require('../controllers/barangController');
+const { upload, tambahBarang, getBarang, getBarangUser, getLifecycleBarang, ambilGratis, hapusBarang } = require('../controllers/barangController');
 
 router.get('/', getBarang);
 router.get('/user/:user_id', getBarangUser);
+router.get('/lifecycle/:barang_id', getLifecycleBarang);
+router.post('/gratis', ambilGratis);
+router.delete('/hapus', hapusBarang);
 router.post('/', upload.single('foto'), tambahBarang);
 
 module.exports = router;
